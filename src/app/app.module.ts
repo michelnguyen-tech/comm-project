2ec1057d3069b18d7a0a89514099b321be507311
Michel Nguyen <michelnguyenm9@gmail.com>
2021-11-21 22:18:49 -0500

    installing bootstrap and beginning header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
