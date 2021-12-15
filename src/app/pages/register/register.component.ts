df77ec059d56a40e6a5dc238549cab87b21d2405
Michel Nguyen <michelnguyenm9@gmail.com>
2021-12-15 00:25:21 -0500

    improve UI and responsiveness of login and signup form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { markedTrigger } from './animations';
 
@@ -11,11 +11,30 @@ import { markedTrigger } from './animations';
   ]
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('month')
+  input!: ElementRef<HTMLInputElement>;
+  years: string[] = ["2021","2020","2019","2018","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2009"];
+  days: string[] = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"];
   
-  constructor(private router: Router) { }
+  constructor(private router: Router, private renderer: Renderer2 ) { }
 
   ngOnInit(): void {
   }
+
+  
+  ngAfterViewInit() {
+    const arr = 
+    ["January","February","March","April","May","June","July","August","May","June","july","August","September","October","November","December"];
+    //using selectRootElement instead of depreaced invokeElementMethod
+    for(var i = 0; i < arr.length; i++) {
+      var option = document.createElement("OPTION");
+      var txt = document.createTextNode(arr[i]);
+      option.appendChild(txt);
+      option.setAttribute("value",arr[i]);
+      this.renderer.insertBefore(this.input.nativeElement ,option, this.input.nativeElement.lastChild);
+    }
+   }
+   
 
   loginForm() {
     this.router.navigateByUrl('/login');
