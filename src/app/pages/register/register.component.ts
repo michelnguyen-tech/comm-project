45ae8d66acf1b1b718691c478f09586ad558bfd3
Michel Nguyen <michelnguyenm9@gmail.com>
2021-12-23 01:06:44 -0500

    adding side-bar styling on hover and on click

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -13,6 +13,7 @@ import { markedTrigger } from './animations';
 export class RegisterComponent implements OnInit {
   @ViewChild('month')
   input!: ElementRef<HTMLInputElement>;
+  
   years: string[] = ["2021","2020","2019","2018","2018","2017","2016","2015","2014","2013","2012","2011","2010","2009","2008","2009"];
   days: string[] = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"];
   
--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,10 +8,26 @@ import { faUsers } from '@fortawesome/free-solid-svg-icons';
 })
 export class SideNavComponent implements OnInit {
   faUsers = faUsers;
+  
+  @ViewChild('groups')
+  groups!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  @ViewChild('chat')
+  chat!: ElementRef<HTMLInputElement>;
+
+  @ViewChild('files')
+  files!: ElementRef<HTMLInputElement>;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  onActive(el: HTMLElement) {
+    this.renderer.removeClass(this.groups.nativeElement, "list-active");
+    this.renderer.removeClass(this.chat.nativeElement, "list-active");
+    this.renderer.removeClass(this.files.nativeElement, "list-active");
+    el.classList.add("list-active");
   }
 
 }
